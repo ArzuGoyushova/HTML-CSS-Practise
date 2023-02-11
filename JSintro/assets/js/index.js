ccb86b4d413774f2ae782ab82df80446aa419627
ArzuGoyushova <arzusg@code.edu.az>
2023-02-11 19:38:51 +0400

    js

---
+++ JSintro/assets/js/index.js
@@ -0,0 +1,23 @@
+// document.writeln("<h1>bollean</h1>")
+// alert("aci")
+// var test=prompt();
+// console.log(test);
+// var isSuccess=confirm("is okay?");
+// console.log(isSuccess);
+//var let const
+// var num=15;
+// num="lorem";
+// console.log(num);
+//var menfi cehet, console ikincini qebul edecek
+//word="lorem"; console log ishleyir. menfi
+// let num=13;
+// num=14;
+
+// const nol=14;
+// console.log(num);
+// if(0){
+//     console.log("lorem")
+// }else{
+//     console.log("ipsum")
+// }
+// falsydi, ipsum qaytarir.
