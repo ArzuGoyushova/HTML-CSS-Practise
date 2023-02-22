357a5b4341177d8fe073e1c572b6340c859e856c
ArzuGoyushova <arzusg@code.edu.az>
2023-02-22 15:07:30 +0400

    updated

--- JSfunc/assets/js/script.js
+++ JSfunc/assets/js/script.js
@@ -183,3 +183,64 @@
 //         break;
 // }
 
+//func declaration arguments
+function sumAllNums() {
+    let sum = 0
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i]
+    }
+    return sum
+  }
+  
+  console.log(sumAllNums(1, 2, 3, 4)) // 10
+
+
+  //arrow func doesn't scope arguments
+const sumAllNums = (...args) => {
+ // console.log(arguments), arguments object not found in arrow function
+ // instead we use a parameter followed by spread operator (...)
+ console.log(args)
+}
+
+sumAllNums(1, 2, 3, 4)
+// [1, 2, 3, 4]
+
+
+// function declaration
+
+const sumAllNums = (...args) => {
+  let sum = 0
+  for (const element of args) {
+    sum += element
+  }
+  return sum
+}
+
+console.log(sumAllNums(1, 2, 3, 4)) // 10
+
+//anonymous
+const anonymousFun = function() {
+    console.log(
+      'I am an anonymous function and my value is stored in anonymousFun'
+    )
+  }
+
+//expression
+//Expression functions are anonymous functions. After we create a function without a name and we assign it to a variable. To return a value from the function we should call the variable
+const square = function(n) {
+    return n * n
+  }
+  
+  console.log(square(2))
+
+//Self invoking functions are anonymous functions which do not need to be called to return a value.
+(function(n) {
+    console.log(n * n)
+  })(2) // 4, but instead of just printing if we want to return and store the data, we do as shown below
+  
+  let squaredNum = (function(n) {
+    return n * n
+  })(10)
+  
+  console.log(squaredNum)
+
