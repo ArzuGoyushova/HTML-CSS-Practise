16be924a647095c445788c58bbce8dade46e8278
ArzuGoyushova <arzusg@code.edu.az>
2023-02-22 15:08:10 +0400

    updated

--- JSarray/assets/js/script.js
+++ JSarray/assets/js/script.js
@@ -110,29 +110,69 @@ let string = "loReM m ";
 // console.log(string.replace(/m/i, "ponchik")); //birincini deyishir, boyuk de olsa
 // console.log(string.replace(/m/gi, "ponchik")); //hamisini deyishir
 
-let arrObj=[
-    {name:"Company1", 
-    createdDate:1996,
-    terminateDate:2000,
-    annualRevenue:5},
-    {name:"Company2", 
-    createdDate:1990,
-    terminateDate:2020,
-    annualRevenue:1000000000},
-    {name:"Company3", 
-    createdDate:1970,
-    terminateDate:2010,
-    annualRevenue:5000},
-    {name:"sompany4", 
-    createdDate:1978,
-    terminateDate:2005,
-    annualRevenue:10000},
-    {name:"Nompany5", 
-    createdDate:1960,
-    terminateDate:1980,
-    annualRevenue:900000}
-]
+// let arrObj=[
+//     {name:"Company1", 
+//     createdDate:1996,
+//     terminateDate:2000,
+//     annualRevenue:5},
+//     {name:"Company2", 
+//     createdDate:1990,
+//     terminateDate:2020,
+//     annualRevenue:1000000000},
+//     {name:"Company3", 
+//     createdDate:1970,
+//     terminateDate:2010,
+//     annualRevenue:5000},
+//     {name:"sompany4", 
+//     createdDate:1978,
+//     terminateDate:2005,
+//     annualRevenue:10000},
+//     {name:"Nompany5", 
+//     createdDate:1960,
+//     terminateDate:1980,
+//     annualRevenue:900000}
+// ]
 
-let revenueSum=arrObj.filter(comp=>comp.name.startsWith("Com") && comp.createdDate>1900 && comp.terminateDate<2025 && comp.annualRevenue>1000).reduce((prev, next)=>prev.annualRevenue+=next.annualRevenue);
+// let revenueSum=arrObj.filter(comp=>comp.name.startsWith("Com") && comp.createdDate>1900 && comp.terminateDate<2025 && comp.annualRevenue>1000).reduce((prev, next)=>prev.annualRevenue+=next.annualRevenue);
 
-console.log(revenueSum);
+// console.log(revenueSum);
+
+// let us check if a banana exist in the array
+
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+let index = fruits.indexOf('banana')  // 0
+
+if(index === -1){
+   console.log('This fruit does not exist in the array')  
+} else {
+    console.log('This fruit does exist in the array')
+}
+// This fruit does exist in the array
+
+// we can use also ternary here
+index === -1 ? console.log('This fruit does not exist in the array'): console.log('This fruit does exist in the array')
+
+// let us check if an avocado exist in the array
+let indexOfAvocado = fruits.indexOf('avocado')  // -1, if the element not found index is -1
+if(indexOfAvocado === -1){
+   console.log('This fruit does not exist in the array')  
+} else {
+    console.log('This fruit does exist in the array')
+}
+// This fruit does not exist in the array
+
+let indexOfLemon = fruits.indexOf('lemon');
+indexOfLemon === -1 ? console.log("Doesn't exist") : console.log("exists");
+
+const numbers = [1, 2, 3, 4, 5, 6]
+numbers.splice(3, 3, 7, 8, 9)
+console.log(numbers.splice(3, 3, 7, 8, 9))  // -> [1, 2, 3, 7, 8, 9] //it removes three item and replace three items
+
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = [frontEnd, backEnd]
+console.log(fullStack)   // [["HTML", "CSS", "JS", "React", "Redux"], ["Node", "Express", "MongoDB"]]
+console.log(fullStack.length)  // 2
+console.log(fullStack[0])  // ["HTML", "CSS", "JS", "React", "Redux"]
+console.log(fullStack[1]) // ["Node", "Express", "MongoDB"]
