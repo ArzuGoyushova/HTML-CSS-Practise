74973fcaff491c29fb0dcfa0a218cb6e4e5477eb
ArzuGoyushova <115929474+ArzuGoyushova@users.noreply.github.com>
2023-04-29 19:21:44 +0400

    updated

--- SQLQuery2904.sql
+++ SQLQuery2904.sql
@@ -23,3 +23,63 @@ Id int IDENTITY PRIMARY KEY,
 
 SELECT * FROM Products
 CROSS JOIN Sizes
+
+CREATE TABLE Grades(
+Id int IDENTITY PRIMARY KEY,
+Letter nvarchar(20),
+MaxGrade int,
+MinGrade int
+)
+
+ALTER TABLE Students Add Mark int
+
+SELECT s.Id, s.Name, s.Surname, s.Mark, g.letter FROM Students s
+JOIN Grades g
+ON
+s.Mark>=g.MinGrade AND s.Mark<=g.MaxGrade
+
+
+--union
+CREATE TABLE OldStudents(
+Id int IDENTITY PRIMARY KEY,
+[Name] nvarchar(50),
+Surname nvarchar(50),
+[Address] nvarchar(50),
+Age int,
+Mark int
+)
+
+SELECT  s.Name, s.Surname FROM Students s
+union all
+SELECT  o.Name, o.Surname FROM OldStudents o
+
+
+--eynilerin 1in saxliyir
+SELECT  s.Name, s.Surname FROM Students s
+union
+SELECT  o.Name, o.Surname FROM OldStudents o
+
+SELECT * FROM Students
+except 
+SELECT * FROM OldStudents
+
+
+SELECT * FROM Students
+intersect 
+SELECT * FROM OldStudents
+
+
+CREATE TABLE Users(
+Id int IDENTITY PRIMARY KEY,
+[Name] nvarchar(50)
+)
+
+INSERT INTO Users VALUES
+('Chuuya'),
+('Chuuya'),
+('Dazai'),
+('Chuuya'),
+('Levi'),
+('Dazai')
+
+SELECT Name, COUNT(NAME) as NameCount FROM Users GROUP BY NAME
