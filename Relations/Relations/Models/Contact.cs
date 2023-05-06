568555b9b35f559fff28716d593fd67e77d69756
ArzuGoyushova <115929474+ArzuGoyushova@users.noreply.github.com>
2023-05-06 16:19:35 +0400

    05.06

---
+++ Relations/Relations/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relations.Models
+{
+    public class Contact
+    {
+        [Key,ForeignKey(nameof(Student))] //Contactin id-ni Student id-den asili edir. hem foreign keydi, hem primary
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
