1c8bf8f7b9231c0d169580a8c34553464d2c0b12
ArzuGoyushova <115929474+ArzuGoyushova@users.noreply.github.com>
2023-05-07 15:43:05 +0400

    07.05 updated

---
+++ Relations/Relations/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relations.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<BookGenre> BookGenre { get; set; }
+
+        public Book()
+        {
+            BookGenre = new List<BookGenre>();
+        }
+    }
+}
--- Relations/Relations/Program.cs
+++ Relations/Relations/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using Relations.Models;
+
+Book book = new();
+book.Id = 1;
+book.Name = "Book1";
+Genre genre = new();
+genre.Id = 2;
+genre.Name = "Genre1";
